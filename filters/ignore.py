0caf75f442bc6820c48e5cf773499aa1c163ba97
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2022-12-13 15:36:11 +0000

    minor

--- filters/ignore.py
+++ filters/ignore.py
@@ -4,9 +4,8 @@
 # Copyright (c) 2017, Aaron Adams <aaron.adams(at)nccgroup(dot)trust>
 # Copyright (c) 2017, Cedric Halbronn <cedric.halbronn(at)nccgroup(dot)trust>
 #
-# Filter for arbitrary names to be used by idahunt.py command line:
-# e.g. idahunt.py --filter "filters/names.py -n Download -e exe -a 32"
-#      idahunt.py --filter "filters/names.py -l 64"
+# Filter for ignoring certain files based on name, extension or content to be used by idahunt.py command line:
+# e.g. idahunt.py --filter "filters/ignore.py"
 
 import argparse
 import os
@@ -29,7 +28,7 @@ def logmsg(s, end=None, debug=True):
 def main(f, cmdline):
     filename, file_extension = os.path.splitext(f)
     # Ignore .c source, .sh bash scripts files
-    if file_extension == ".c" or file_extension == ".sh":
+    if file_extension == ".c" or file_extension == ".sh"  or file_extension == ".txt":
         return None
     fd = open(f, "rb")
     data = fd.read(len(b"#!/bin/sh"))
